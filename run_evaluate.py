c823af8a9a3996ec8e4199020fa2e6c818eb9024
欧阳戈亮 <113326936+Ouyangliangge@users.noreply.github.com>
2024-10-30 10:52:12 +0800

    Add files via upload

---
+++ run_evaluate.py
@@ -0,0 +1,44 @@
+import argparse
+from pathlib import Path
+import os
+from core.chat_manager import ChatManager
+from viseval import Dataset, Evaluator
+from langchain_openai import ChatOpenAI
+
+def _main():
+
+    # config vision model
+    vision_model = ChatOpenAI(
+        api_key=os.getenv("OPENAI_API_KEY"),
+        base_url=os.getenv("OPENAI_API_BASE"),
+        model_name="gpt-4-vision-preview",
+        max_retries=999,
+        temperature=0.0,
+        request_timeout=20,
+        max_tokens=4096,
+    )
+
+    folder = "E:/visEval_dataset"
+    library = 'matplotlib'
+    webdriver = Path("C:\Program Files\Google\Chrome\Application\chromedriver.exe")
+    log_folder = Path("evaluate_logs")
+
+    # config dataset
+    # dataset = Dataset(folder, table_type, with_irrelevant_tables)
+    dataset = Dataset(Path(folder))
+    # config agent # API config in api_config.py
+    agent = ChatManager(data_path=folder,log_path="E:/mac-vis/test_logs.txt",)
+
+    # config evaluator
+    # evaluator = Evaluator(webdriver_path=webdriver, vision_model=vision_model)
+    evaluator = Evaluator()
+    # evaluate agent
+    config = {"library": library, "logs": log_folder}
+    result = evaluator.evaluate(agent, dataset, config)
+    print(result)
+    score = result.score()
+    print(f"Score: {score}")
+
+
+if __name__ == "__main__":
+    _main()
