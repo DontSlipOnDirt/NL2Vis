2e90f5eb8d4c67a883d50c72269b7d037cb28ea4
欧阳戈亮 <113326936+Ouyangliangge@users.noreply.github.com>
2024-12-02 13:32:27 +0800

    Update api_config.py

--- core/api_config.py
+++ core/api_config.py
@@ -1,13 +1,14 @@
 import os
 # set your AZURE_OPENAI_API_BASE, AZURE_OPENAI_API_KEY here!
-API_KEY = "5FGygbhO86laoGOvGJOST59kIYNJfpvLPr5pVBl4eexMHbkgl56DJQQJ99AKACi0881XJ3w3AAABACOGNMr3"
-# API_KEY = "sk-wrdbEoFKz2IIEMky3yuH5HcPIyKA050aJCK847QeafnrXQ4L" #付费key
+API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 os.environ["AZURE_OPENAI_API_KEY"] = API_KEY
 
-# API_BASE = "https://api.chatanywhere.tech/v1"
+# OPENAI_API_KEY = "XXXXXXXXXXXXXXXXXXXX"
+# os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+# API_BASE = "XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 # os.environ["OPENAI_API_BASE"] = API_BASE
 
-AZURE_OPENAI_ENDPOINT = "https://nv-agent.openai.azure.com/"
+AZURE_OPENAI_ENDPOINT = "https://XXXXXXXX.openai.azure.com/"
 OPENAI_API_VERSION = "2024-02-01"
 os.environ["AZURE_OPENAI_ENDPOINT"] = AZURE_OPENAI_ENDPOINT
 os.environ["OPENAI_API_VERSION"] = OPENAI_API_VERSION
